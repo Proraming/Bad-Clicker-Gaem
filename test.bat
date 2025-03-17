782f09d2857e267b11423f354b5bc4179cff7578
Proraming <mrluckyness1@gmail.com>
2025-03-16 20:57:24 -0600

    Update test.bat

--- test.bat
+++ test.bat
@@ -2,4 +2,4 @@ copy .\src\lib\UI.lua .\editor\UI.lua
 xcopy /E /I /H /Y .\src\assets .\editor\assets
 
 "C:\Program Files\LOVE\love.exe" ./editor/  --console
-# in the name of the commenter
+# in the name of the GREAT person
